4b7180639d9855c6e27a1f132b2aa8aeee71fab1
thank309 <131947740+thank309@users.noreply.github.com>
2023-05-01 13:49:25 +0700

    Add files via upload

---
+++ newcode.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+
+struct students{                //DATA STORES INFORMATIONS OF A STUDENT 
+    char id[20];                                           
+    char first_name[20];
+    char last_name[20];
+    char birth[20];
+    float al;
+    float cal;
+    float basic;
+    float gpa;
+};
+
+int main(){
+    int num_student;
+    float highest, lowest, hbp; 
+    do{                       //A LOOP ENSURES THAT USERS HAVE TO INPUT A NUMBER SMALLER THAN 1000
+        printf("\nThe numbers of students(Max 1000) is: ");     
+        scanf("%d", &num_student);
+        if(num_student>1000){
+            printf("Outta range!!!\nPlease try again with a number smaller than 1000");
+            
+        }; 
+    }while(num_student>1000);
+    struct students student[num_student];
+    for ( int i=0; i < num_student; i++){
+        printf("\nThe ID student %d is: ",i+1);
+        scanf("%s", student[i].id);
+        printf("The first name of student %d is: ",i+1);
+        scanf("%s", student[i].first_name);
+        printf("The last name of student %d is: ",i+1);
+        scanf("%s", student[i].last_name);
+        printf("What's your birthday(DD/MM/YYYY): ");
+        scanf("%s", student[i].birth);
+        printf("The score Alebra of student %d is: ",i+1);
+        scanf("%f", &student[i].al);
+        printf("The score Calculus of student %d is: ",i+1);
+        scanf("%f", &student[i].cal);
+        printf("The score Basic Programming of student %d is: ",i+1);
+        scanf("%f", &student[i].basic);
+        student[i].gpa = (student[i].al + student[i].basic + student[i].cal)/3;
+        strcat(student[i].last_name," ");     //This merges a space to the end of str 
+    }
+
+    //Print the header as the follwing code
+    printf("\n%-15s%-15s%-15s%-12s%-12s%-20s%s\n", "StudentID","Full Name","Birthdate","Algebra","Calculus","Basic Programming","GPA");
+    printf("----------------------------------------------------------------------------------------------\n");
+    
+    //Print informations of each student
+    for( int i=0; i< num_student; i++){
+        printf("%-15s%-15s%-15s%-12.1f%-12.1f%-20.1f%0.1f\n",student[i].id, strcat(student[i].last_name, student[i].first_name),
+        student[i].birth, student[i].al, student[i].cal, student[i].basic, student[i].gpa);
+    };
+    
+    //Print output to a file
+    FILE *f = fopen("Lists of Students.txt", "w");
+    fprintf(f,"\n%-15s%-15s%-15s%-12s%-12s%-20s%s\n", "StudentID","Full Name","Birthdate","Algebra","Calculus","Basic Programming","GPA");
+    fprintf(f,"----------------------------------------------------------------------------------------------\n");
+    for( int i=0; i< num_student; i++){
+        fprintf(f, "%-15s%-15s%-15s%-12.1f%-12.1f%-20.1f%0.1f\n",student[i].id, strcat(student[i].last_name, student[i].first_name),
+        student[i].birth, student[i].al, student[i].cal, student[i].basic, student[i].gpa);
+    }
+    fclose(f);
+
+
+    highest = 0;    
+    for (int i=0; i < num_student; i++){     //Find the highest GPA
+        if (student[i].gpa > highest){
+            highest = student[i].gpa;
+        }
+    }
+   
+    lowest = 100;
+    for (int i=0; i < num_student; i++){     //Find the lowest GPA
+        if (student[i].gpa < lowest){
+            lowest = student[i].gpa ;
+        }
+    }
+    hbp = 0;
+    for (int i=0; i < num_student; i++){
+        if (student[i].basic > hbp){
+            hbp = student[i].basic;  
+            }
+    }
+    
+    return 0;
+}
